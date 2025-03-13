c4b1e0107081c05fbcd96786cbcbfb746c7d5b69
Vitor Jau <vitor.jau@dtidigital.com.br>
2025-03-12 21:29:01 -0300

    teste 2

--- carro.java
+++ carro.java
@@ -2,6 +2,6 @@ public class carro{
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
-        }
+            System.out.println("ola mundo");
     }
 }
