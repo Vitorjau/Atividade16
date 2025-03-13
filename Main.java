0eb5e24c2b3bd5f61cda8d3865743b7cb754a619
Isaque Roberto <71456433+IsaqueR02@users.noreply.github.com>
2025-03-12 22:08:23 -0300

    Update and rename main.java to Main.java modificated

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+
+    public static void main(String[] args) {
+        Veiculos veiculo = new Veiculos();
+        Moto moto = new Moto();
+        Carro carro = new Carro();
+        
+        int automovel = 0;
+    
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+    
+                automovel++;
+            }
+    
+            System.out.println("Quantidade de veiculos: " + automovel);
+        }
+}
