19a8b3fb6b3a86dcf7913e1a5cd728eabe7b48e6
Isaque Roberto <isaquezulato@gmail.com>
2025-03-13 21:42:19 -0300

    adicionado atributos do veiculo

--- Veiculos.java
+++ Veiculos.java
@@ -1,3 +1,12 @@
 public class Veiculos {
+    String marca;
+    String modelo;
+    String cor;
+    int ano;
+
+    public int getAutomovel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAutomovel'");
+    }
     
 }
