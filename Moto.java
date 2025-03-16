895545c33a6bfcd68dbde07ec8d5911ec06234a4
Isaque Roberto <isaquezulato@gmail.com>
2025-03-16 12:08:34 -0300

    Herança da classe carros no Moto
    herança os atributos e comportamentos da classe Carros

--- Moto.java
+++ Moto.java
@@ -1,13 +1,14 @@
-public class Moto {
+public class Moto extends Carros{
+    Veiculos moto;
+    String[] marca = {"Honda", "Yamaha", "Suzuki", "Kawasaki", "Ducati", "BMW", "Triumph", "Harley-Davidson", "KTM", "Indian"};
+    String[] modelo = {"CB 300", "XRE 300", "Burgman", "Ninja", "Monster", "S 1000 RR", "Street Triple", "Iron 883", "Duke", "Scout"};
+    String[] cor = {"Branco", "Preto", "Prata", "Vermelho", "Azul", "Verde", "Amarelo", "Laranja", "Roxo", "Marrom"};
+    int[] ano = {2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019};
+    int rodas = 2;
 
-    Carro carro = new Carro();
-    Veiculos veiculo = new Veiculos();
-
-    moto = carro;
-    
-    public int getAutomovel() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAutomovel'");
+    public Moto(Veiculos moto) {
+        super(moto); // Call to the superclass constructor
+        this.moto = moto;
+        moto = carro;
     }
-    
 }
